c6d25eb4f031b5c4d3e18b1d6b5178a93e703ae8
Erik Grubbs <erikgrubbs@gmail.com>
2019-09-28 12:00:48 -0700

    Completes Step 1

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 // TODO
+
+const app = express();
+const port = 3003;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.listen(port, console.log(`${port} is listening`))
